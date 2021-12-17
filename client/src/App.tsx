f2e11f0366e141418df9f5ea1aa15a7e6722829b
Gabe <gabriel.riera@sharesight.co.nz>
2021-12-17 23:49:08 +1300

    chore: cleanup react boilerplace and remove unecessary things

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
+import type { VFC } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App: VFC = () => <div>React Leaderboard</div>
